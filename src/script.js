62bc2354f0cf62e5eda5681939709c4d651c8395
Pauline Oliveira <apaulineoliveira@gmail.com>
2024-09-18 18:42:01 -0300

    Refactor/ refatorando todo o layout da funcionalidade

--- src/script.js
+++ src/script.js
@@ -1,12 +1,31 @@
 function generateQRCode() {
-    let website = document.getElementById("website").value; 
-    if (website) {
-       let qrcodeContainer = document.getElementById("qrcode");
-       qrcodeContainer.innerHTML = "";
-       new QRCode(qrcodeContainer, website);
-       document.getElementById("qrcode-container").style.display= "block";
-    } else {
-        alert("Insira uma URL");
-       }
-     }
+  let website = document.getElementById("website").value; 
+  if (website) {
+    let qrcodeContainer = document.getElementById("qrcode");
+    qrcodeContainer.innerHTML = "";
+    new QRCode(qrcodeContainer, website);
     
+  
+    document.getElementById("qrcode-container").style.display = "block";
+    document.getElementById("new-qrcode-btn").style.display = "inline-block";
+    
+   
+    document.querySelector('button[onclick="generateQRCode()"]').style.display = "none";
+    
+  } else {
+    showAlert("Ops... Primeiro insira uma URL válida.");
+  }
+}
+
+function showAlert(message) {
+  document.getElementById("alert-message").innerText = message;
+  document.getElementById("custom-alert").style.display = "flex";
+}
+
+function closeAlert() {
+  document.getElementById("custom-alert").style.display = "none";
+}
+
+function reloadPage() {
+  location.reload();
+}
